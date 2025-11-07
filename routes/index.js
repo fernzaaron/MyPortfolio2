e450e3fc7fccd67ee775b184b2eccbf1a850df14
fernzaaron <aaron.fernandez@ontariotechu.net>
2025-11-06 20:25:01 -0500

    comments + minimal fixes on views folder

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
+// simple routes for my infr3120 portfolio
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  // render the home page
   res.render('home', { title: 'Home' });
 });
 
 /* Optional: GET /home as alias */
 router.get('/home', function(req, res, next) {
+  // another route for home just /home
   res.render('home', { title: 'Home' });
 });
 
 /* GET about page. */
 router.get('/about', function(req, res, next) {
+  // about page (info about me)
   res.render('about', { title: 'About' });
 });
 
 /* GET projects page. */
 router.get('/projects', function(req, res, next) {
+  // projects list (short summaries)
   res.render('projects', { title: 'Projects' });
 });
 
 /* GET contact page. */
 router.get('/contact', function(req, res, next) {
+  // contact page (form front-end)
   res.render('contact', { title: 'Contact' });
 });
 
